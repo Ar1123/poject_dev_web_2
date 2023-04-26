7419fb871d6e68195738e3c5db53618718364ad9
andre <amestr367@gmail.com>
2023-04-26 17:48:34 -0500

    metodos get, getbyId, put

--- src/main/java/com/example_project/project/entity/ServerEntity.java
+++ src/main/java/com/example_project/project/entity/ServerEntity.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
